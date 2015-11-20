0c8ec8b366ad2ff8e4d2d19e0325a9e5eff88b93
Lennart Landsmeer <lennart@landsmeer.email>
2015-11-20 22:10:23 +0100

    check next state

--- virtual-dom-remote-mount.js
+++ virtual-dom-remote-mount.js
@@ -1,6 +1,9 @@
+var diff = require('virtual-dom/diff');
+
 function Page() {
     this.state = {};
     this.init();
+    this.tree = null;
 }
 
 Page.prototype.render = function(state) {
@@ -10,10 +13,18 @@ Page.prototype.init = function(state) {
 };
 
 Page.prototype.setState = function(nextState) {
+    var key;
     if (typeof nextState === 'function') {
         nextState = nextState(this.state);
     }
-    this.state = nextState;
+    if (typeof nextState !== 'object') {
+        throw 'nextState is not or does not return an object';
+    }
+    for (key in nextState) {
+        if (nextState.hasOwnProperty(key)) {
+            this.state[key] = nextState[key];
+        }
+    }
     this.forceUpdate();
 };
 
